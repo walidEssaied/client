c17ce6e3334a9e518efb5478f7681a56556f5de9
walidEssaied <walidessaied01@gmail.com>
2023-04-11 22:42:03 +0100

    update api

--- src/services/in/index.ts
+++ src/services/in/index.ts
@@ -4,17 +4,17 @@ export const getIns =
   () =>
     async (): Promise<[]> => {
       return await axiosInstance
-        .get("http://localhost:1337/api/ins?populate=*")
+        .get("https://server-a1ho.onrender.com/api/ins?populate=*")
         .then(({ data }) => data.data);
     };
 
 
 export const CreateIn =
   async (data: any): Promise<unknown> => {
-    return await axiosInstance.post(`http://localhost:1337/api/ins`, { data: data }).then(({ data }) => data);
+    return await axiosInstance.post(`https://server-a1ho.onrender.com/api/ins`, { data: data }).then(({ data }) => data);
   }
 
 export const updateIn =
   async (data: any): Promise<unknown> => {
-    return await axiosInstance.put(`http://localhost:1337/api/ins/${data.id}`, { data: data.attributes }).then(({ data }) => data);
+    return await axiosInstance.put(`https://server-a1ho.onrender.com/api/ins/${data.id}`, { data: data.attributes }).then(({ data }) => data);
   }
